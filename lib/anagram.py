568e86e1d57cae9116347f93363e62ae8d69f0ad
OumaArera <johnouma999@gmail.com>
2024-02-29 18:16:59 +0300

    feat: assignment done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        matches = []
+        sorted_word = sorted(self.word)
+        for anagram in possible_anagrams:
+            if len(anagram) == len(self.word) and sorted(anagram.lower()) == sorted_word:
+                matches.append(anagram)
+        return matches
